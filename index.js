dbc027af742d6c9a0078c0e2feeaf993a72600c6
kelvin_kiprop <kelkiprop@gmail.com>
2024-11-24 15:59:41 +0300

    functions lab

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(myString) {
+    return myString.toUpperCase();
+}
+function whisper(myString) {
+    return myString.toLowerCase();
+}
+function logShout(myString) {
+
+    console.log(shout(myString));
+}
+function logWhisper(myString) {
+    console.log(whisper(myString));
+}
+console.log(shout(mystring));
+function sayHiToHeadphonedRoommate(myString) {
+    if (myString === myString.toLowerCase()) {
+        return "I can't hear you!";
+    }
+}
+function sayHiToHeadphonedRoommate(myString) {
+    if (myString === myString.toUpperCase()) {
+        return "YES INDEED!";
+    }
+}
+function sayHiToHeadphonedRoommate(myString) {
+    if (myString === myString.toLowerCase()) {
+        return "I can't hear you!";
+    }
+    else if (myString === myString.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    else if (myString === "Let's have dinner together!") {
+        return "I would love to!"
+    }
+}
